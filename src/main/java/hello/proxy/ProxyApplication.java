b81b2356c2183ab40c7938b1b219d5b24752002f
Msukoo <rnalsttnn@gmail.com>
2022-07-23 18:29:05 +0900

    예제 v2

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) //클래스를 스프링 빈으로 등록한다.
+//@Import(AppV1Config.class) //클래스를 스프링 빈으로 등록한다.
+@Import({AppV1Config.class, AppV2Config.class}) //클래스를 스프링 빈으로 등록한다.
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 위 Import 클래스와 component Scan 대상 패키지를 분리하기 위해서
 public class ProxyApplication {
 
