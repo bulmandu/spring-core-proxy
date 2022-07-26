35076cf9fdd8a0ad3503026a0a2aad15b5ff8ce1
Msukoo <rnalsttnn@gmail.com>
2022-07-26 21:17:28 +0900

    test: proxy pattern 예제2(proxy 적용)

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,18 @@ public class ProxyPatternTest {
     void noProxyTest(){
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+
+        // 클라이언트에 캐시 프록시를 의존관계 주입해줌 (위 테스트코드와 비교)
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
