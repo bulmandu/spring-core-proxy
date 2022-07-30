66a0965bb789a451e1a458b01b6114175366c4e0
Msukoo <rnalsttnn@gmail.com>
2022-07-30 17:52:49 +0900

    feat: LogTrace jdk동적 프록시 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class) //클래스를 스프링 빈으로 등록한다.
 //@Import({AppV1Config.class, AppV2Config.class}) //클래스를 스프링 빈으로 등록한다.
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 위 Import 클래스와 component Scan 대상 패키지를 분리하기 위해서
 public class ProxyApplication {
 
