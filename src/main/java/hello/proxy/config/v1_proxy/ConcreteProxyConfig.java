465927275c822a19c418869e6668064f01db0320
Msukoo <rnalsttnn@gmail.com>
2022-07-30 16:19:16 +0900

    test: jdk동적프록시-reflection

--- src/main/java/hello/proxy/config/v1_proxy/ConcreteProxyConfig.java
+++ src/main/java/hello/proxy/config/v1_proxy/ConcreteProxyConfig.java
@@ -10,6 +10,9 @@ import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ *
+ */
 @Configuration
 public class ConcreteProxyConfig {
 
